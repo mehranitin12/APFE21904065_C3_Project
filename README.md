ddd666e8151dae6e0382fc7c383c73d8f857924a
Nitin Mehra <68689563+mehranitin12@users.noreply.github.com>
2021-06-28 01:11:01 +0530

    Part 2: Solution
    Part 2: Solution

---
+++ README.md
@@ -0,0 +1,2 @@
+# APFE21904065_C3_Project
+ This is Restaurant Finder Project which contains Part 2 and Part 3.
---
+++ src/main/java/RestaurantService.java
@@ -0,0 +1,41 @@
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RestaurantService {
+    private static List<Restaurant> restaurants = new ArrayList<>();
+
+    public Restaurant findRestaurantByName(String restaurantName) {
+
+        for (Restaurant res : restaurants) {
+            if (res.getName().equals(restaurantName)) {
+                return res;
+            } //else {
+//                try {
+//                    throw new restaurantNotFoundException("Restaurant could not be found");
+//                } catch (restaurantNotFoundException e) {
+//                    e.printStackTrace();
+//                }
+           // }
+        }
+        return null;
+        //DELETE ABOVE STATEMENT AND WRITE CODE HERE
+    }
+
+
+    public Restaurant addRestaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
+        Restaurant newRestaurant = new Restaurant(name, location, openingTime, closingTime);
+        restaurants.add(newRestaurant);
+        return newRestaurant;
+    }
+
+    public Restaurant removeRestaurant(String restaurantName) throws restaurantNotFoundException {
+        Restaurant restaurantToBeRemoved = findRestaurantByName(restaurantName);
+        restaurants.remove(restaurantToBeRemoved);
+        return restaurantToBeRemoved;
+    }
+
+    public List<Restaurant> getRestaurants() {
+        return restaurants;
+    }
+}
